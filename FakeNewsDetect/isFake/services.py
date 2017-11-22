8d30981c468a6a33fd767aa6a000bd7d6eaf4dec
Gilad Lumbroso <gullumbroso@gmail.com>
2017-11-22 02:50:03 +0200

    no matplotlib

--- FakeNewsDetect/isFake/services.py
+++ FakeNewsDetect/isFake/services.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -137,9 +136,6 @@ def get_trained_machine(present_graph):
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-    if present_graph:
-        plot_decision_graph(X_train_tfidf, articles)
-
     # Train the classifier
     # clf = MultinomialNB().fit(X_train_tfidf, articles.target)
     # clf = SGDClassifier().fit(X_train_tfidf, articles.target)
@@ -208,59 +204,3 @@ def get_precision():
 
     print(np.mean(predicted == articles.target))
     print(metrics.classification_report(articles.target, predicted, target_names=articles.target_names))
-
-
-def plot_decision_graph(X_, data):
-
-    # import some data to play with
-    X = X_[:, :2] # we only take the first two features. We could
-    # avoid this ugly slicing by using a two-dim dataset
-    y = data.target
-
-    h = .02  # step size in the mesh
-
-    # we create an instance of SVM and fit out data. We do not scale our
-    # data since we want to plot the support vectors
-    C = 1.0  # SVM regularization parameter
-    svc = svm.SVC(kernel='linear', C=C).fit(X, y)
-    rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
-    poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
-    lin_svc = svm.LinearSVC(C=C).fit(X, y)
-
-    # create a mesh to plot in
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-
-    # title for the plots
-    titles = ['SVC with linear kernel',
-              'LinearSVC (linear kernel)',
-              'SVC with RBF kernel',
-              'SVC with polynomial (degree 3) kernel']
-
-    for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        plt.subplot(2, 2, i + 1)
-        plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-
-        # Plot also the training points
-        x_first = np.array(X[:, 0])
-        x_last = np.array(X[:, 1])
-        plt.scatter(x_first, x_last, c=y, cmap=plt.cm.coolwarm)
-        plt.xlabel('Sepal length')
-        plt.ylabel('Sepal width')
-        plt.xlim(xx.min(), xx.max())
-        plt.ylim(yy.min(), yy.max())
-        plt.xticks(())
-        plt.yticks(())
-        plt.title(titles[i])
-
-    plt.show()
